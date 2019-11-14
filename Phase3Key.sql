84e7e4665441aaf503a33fb69fe18f812f615582
David Zhou <dzhou71@gatech.edu>
2019-11-14 14:24:34 -0500

    Phase 3 SQL + Misc Files
    I added all of the most current data and mapped an enitrely new create statement directly from the schema key

---
+++ Phase3Key.sql
@@ -0,0 +1,355 @@
+CREATE DATABASE  IF NOT EXISTS `team11` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `team11`;
+-- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: team11
+-- ------------------------------------------------------
+-- Server version	8.0.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `admin` (
+  `username` varchar(30) NOT NULL,
+  PRIMARY KEY (`username`),
+  CONSTRAINT `fk_admin_emp` FOREIGN KEY (`username`) REFERENCES `employee` (`username`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `admin`
+--
+
+LOCK TABLES `admin` WRITE;
+/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `company`
+--
+
+DROP TABLE IF EXISTS `company`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `company` (
+  `comName` varchar(20) NOT NULL,
+  PRIMARY KEY (`comName`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `company`
+--
+
+LOCK TABLES `company` WRITE;
+/*!40000 ALTER TABLE `company` DISABLE KEYS */;
+/*!40000 ALTER TABLE `company` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customer`
+--
+
+DROP TABLE IF EXISTS `customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `customer` (
+  `username` varchar(30) NOT NULL,
+  PRIMARY KEY (`username`),
+  CONSTRAINT `fk_cust_user` FOREIGN KEY (`username`) REFERENCES `user` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customer`
+--
+
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customercreditcard`
+--
+
+DROP TABLE IF EXISTS `customercreditcard`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `customercreditcard` (
+  `creditCardNum` int(11) NOT NULL,
+  `username` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`creditCardNum`),
+  KEY `fk_custcredit_cust_idx` (`username`),
+  CONSTRAINT `fk_custcredit_cust` FOREIGN KEY (`username`) REFERENCES `customer` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customercreditcard`
+--
+
+LOCK TABLES `customercreditcard` WRITE;
+/*!40000 ALTER TABLE `customercreditcard` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customercreditcard` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customerviewmovie`
+--
+
+DROP TABLE IF EXISTS `customerviewmovie`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `customerviewmovie` (
+  `creditCardNum` int(11) NOT NULL,
+  `thName` varchar(45) NOT NULL,
+  `comName` varchar(45) NOT NULL,
+  `movName` varchar(45) NOT NULL,
+  `movReleaseDate` date NOT NULL,
+  `movPlayDate` date NOT NULL,
+  PRIMARY KEY (`creditCardNum`,`thName`,`comName`,`movName`,`movReleaseDate`,`movPlayDate`),
+  KEY `fk_custview_movplay_idx` (`thName`,`comName`,`movName`,`movReleaseDate`,`movPlayDate`),
+  CONSTRAINT `fk_custview_custcredit` FOREIGN KEY (`creditCardNum`) REFERENCES `customercreditcard` (`creditCardNum`),
+  CONSTRAINT `fk_custview_movplay` FOREIGN KEY (`thName`, `comName`, `movName`, `movReleaseDate`, `movPlayDate`) REFERENCES `movieplay` (`thName`, `comName`, `movName`, `movReleaseDate`, `movPlayDate`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customerviewmovie`
+--
+
+LOCK TABLES `customerviewmovie` WRITE;
+/*!40000 ALTER TABLE `customerviewmovie` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customerviewmovie` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `employee`
+--
+
+DROP TABLE IF EXISTS `employee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `employee` (
+  `username` varchar(30) NOT NULL,
+  PRIMARY KEY (`username`),
+  CONSTRAINT `fk_emp_user` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employee`
+--
+
+LOCK TABLES `employee` WRITE;
+/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `manager`
+--
+
+DROP TABLE IF EXISTS `manager`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `manager` (
+  `username` varchar(30) NOT NULL,
+  `comName` varchar(45) NOT NULL,
+  `manStreet` varchar(45) NOT NULL,
+  `manCity` varchar(45) NOT NULL,
+  `manState` varchar(2) NOT NULL,
+  `manZipcode` int(11) NOT NULL,
+  PRIMARY KEY (`username`),
+  KEY `comName_idx` (`comName`),
+  CONSTRAINT `fk_man_com` FOREIGN KEY (`comName`) REFERENCES `company` (`comName`),
+  CONSTRAINT `fk_man_emp` FOREIGN KEY (`username`) REFERENCES `employee` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `manager`
+--
+
+LOCK TABLES `manager` WRITE;
+/*!40000 ALTER TABLE `manager` DISABLE KEYS */;
+/*!40000 ALTER TABLE `manager` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `movie`
+--
+
+DROP TABLE IF EXISTS `movie`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `movie` (
+  `movName` varchar(30) NOT NULL,
+  `movReleaseDate` date NOT NULL,
+  `duration` int(11) NOT NULL,
+  PRIMARY KEY (`movName`,`movReleaseDate`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `movie`
+--
+
+LOCK TABLES `movie` WRITE;
+/*!40000 ALTER TABLE `movie` DISABLE KEYS */;
+/*!40000 ALTER TABLE `movie` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `movieplay`
+--
+
+DROP TABLE IF EXISTS `movieplay`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `movieplay` (
+  `thName` varchar(30) NOT NULL,
+  `comName` varchar(45) NOT NULL,
+  `movName` varchar(45) NOT NULL,
+  `movReleaseDate` date NOT NULL,
+  `movPlayDate` date NOT NULL,
+  PRIMARY KEY (`thName`,`comName`,`movName`,`movReleaseDate`,`movPlayDate`),
+  KEY `fk_movplay_mov_idx` (`movName`,`movReleaseDate`),
+  CONSTRAINT `fk_movplay_mov` FOREIGN KEY (`movName`, `movReleaseDate`) REFERENCES `movie` (`movName`, `movReleaseDate`),
+  CONSTRAINT `fk_movplay_th` FOREIGN KEY (`thName`, `comName`) REFERENCES `theater` (`thName`, `comName`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `movieplay`
+--
+
+LOCK TABLES `movieplay` WRITE;
+/*!40000 ALTER TABLE `movieplay` DISABLE KEYS */;
+/*!40000 ALTER TABLE `movieplay` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `theater`
+--
+
+DROP TABLE IF EXISTS `theater`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `theater` (
+  `thName` varchar(30) NOT NULL,
+  `comName` varchar(30) NOT NULL,
+  `capacity` int(11) NOT NULL,
+  `thStreet` varchar(45) NOT NULL,
+  `thCity` varchar(45) NOT NULL,
+  `thState` varchar(45) NOT NULL,
+  `thZipcode` int(11) NOT NULL,
+  `manUsername` varchar(45) NOT NULL,
+  PRIMARY KEY (`thName`,`comName`),
+  KEY `fk_th_comp_idx` (`comName`),
+  KEY `fk_th_man_idx` (`manUsername`),
+  CONSTRAINT `fk_th_comp` FOREIGN KEY (`comName`) REFERENCES `company` (`comName`),
+  CONSTRAINT `fk_th_man` FOREIGN KEY (`manUsername`) REFERENCES `manager` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `theater`
+--
+
+LOCK TABLES `theater` WRITE;
+/*!40000 ALTER TABLE `theater` DISABLE KEYS */;
+/*!40000 ALTER TABLE `theater` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `username` varchar(30) NOT NULL,
+  `status` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `firstname` varchar(45) NOT NULL,
+  `lastname` varchar(45) NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `uservisittheater`
+--
+
+DROP TABLE IF EXISTS `uservisittheater`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `uservisittheater` (
+  `visitID` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `thName` varchar(45) NOT NULL,
+  `comName` varchar(45) NOT NULL,
+  `visitDate` date NOT NULL,
+  PRIMARY KEY (`visitID`),
+  KEY `fk_uservisit_user_idx` (`username`),
+  KEY `fk_uservisit_th_idx` (`thName`,`comName`),
+  CONSTRAINT `fk_uservisit_th` FOREIGN KEY (`thName`, `comName`) REFERENCES `theater` (`thName`, `comName`),
+  CONSTRAINT `fk_uservisit_user` FOREIGN KEY (`username`) REFERENCES `user` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `uservisittheater`
+--
+
+LOCK TABLES `uservisittheater` WRITE;
+/*!40000 ALTER TABLE `uservisittheater` DISABLE KEYS */;
+/*!40000 ALTER TABLE `uservisittheater` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'team11'
+--
+
+--
+-- Dumping routines for database 'team11'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-11-14 14:19:55
