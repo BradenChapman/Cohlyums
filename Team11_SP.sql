d0acf2f39cbd8a801c35805ddaa26b542cadaae7
maryberrie <changx964@gmail.com>
2019-11-18 22:19:18 -0500

    creating Team11_SP.sql file

---
+++ Team11_SP.sql
@@ -0,0 +1,109 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `user_login`(IN i_username VARCHAR(50), IN i_password VARCHAR(50))
+BEGIN
+	SELECT username, status, isCustomer, i_username in (
+		SELECT username
+        FROM admin
+    ) as isAdmin, i_username in (
+		SELECT username
+        FROM manager
+    ) as isManager
+	FROM user
+	WHERE i_username = user.username and i_password = user.password;
+END
+
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `user_register`(IN i_username VARCHAR(50), IN i_password VARCHAR(50), IN i_firstname VARCHAR(50), IN i_lastname VARCHAR(50))
+BEGIN
+		INSERT INTO user (username, password, firstname, lastname) VALUES (i_username, MD5(i_password), i_firstname, i_lastname);
+END
+
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `customer_only_register`(IN i_username VARCHAR(50), IN i_password VARCHAR(50), IN i_firstname VARCHAR(50), IN i_lastname VARCHAR(50))
+BEGIN
+		INSERT INTO user (username, password, firstname, lastname, isCustomer) VALUES (i_username, MD5(i_password), i_firstname, i_lastname,1);
+END
+
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `customer_add_credicard`(IN i_username VARCHAR(50), IN i_creditCardNum CHAR(16))
+BEGIN
+		INSERT INTO customercreditcard (username, creditCardNum) VALUES (i_username, i_creditCardNum);
+END
+
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `manager_only_register`(IN i_username VARCHAR(50), IN i_password VARCHAR(50), IN i_firstname VARCHAR(50), IN i_lastname VARCHAR(50), IN i_comName VARCHAR(50), IN i_empStreet VARCHAR(50), IN i_empCity VARCHAR(50), IN i_empState CHAR(2), IN i_empZipcode CHAR(5))
+BEGIN
+		INSERT INTO user (username, password, firstname, lastname) VALUES (i_username, MD5(i_password), i_firstname, i_lastname);
+        INSERT INTO manager (username, comName, manStreet, manCity, manState, manZipcode) VALUES (i_username, i_comName, i_empStreet, i_empCity, i_empState, i_empZipcode);
+END
+
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `manager_customer_register`(IN i_username VARCHAR(50), IN i_password VARCHAR(50), IN i_firstname VARCHAR(50), IN i_lastname VARCHAR(50), IN i_comName VARCHAR(50), IN i_empStreet VARCHAR(50), IN i_empCity VARCHAR(50), IN i_empState CHAR(2), IN i_empZipcode CHAR(5))
+BEGIN
+		INSERT INTO user (username, password, firstname, lastname, isCustomer) VALUES (i_username, MD5(i_password), i_firstname, i_lastname,1);
+        INSERT INTO manager (username, comName, manStreet, manCity, manState, manZipcode) VALUES (i_username, i_comName, i_empStreet, i_empCity, i_empState, i_empZipcode);
+END
+
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `manager_customer_add_creditcard`(IN i_username VARCHAR(50), IN i_creditCardNum CHAR(16))
+BEGIN
+		INSERT INTO customercreditcard (username, creditCardNum) VALUES (i_username, i_creditCardNum);
+END
+
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `admin_approve_user`(IN i_username VARCHAR(50))
+BEGIN
+		UPDATE user
+        SET status = 'Approved'
+        WHERE username = i_username;
+END
+
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `admin_decline_user`(IN i_username VARCHAR(50))
+BEGIN
+		UPDATE user
+        SET status = 'Declined'
+        WHERE username = i_username;
+END
+
+
+-- Last one done - Screen 13: Admin decline user
+
+-- INSERT YOUR STUFF HERE
+
+-- Next procedure - Screen 22: User filter theater
+
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `user_filter_th`(IN i_thName VARCHAR(50), IN i_comName VARCHAR(50), IN i_city VARCHAR(50), IN i_state VARCHAR(3))
+BEGIN
+    DROP TABLE IF EXISTS UserFilterTh;
+    CREATE TABLE UserFilterTh
+	SELECT thName, thStreet, thCity, thState, thZipcode, comName 
+    FROM Theater
+    WHERE 
+		(thName = i_thName OR i_thName = "ALL") AND
+        (comName = i_comName OR i_comName = "ALL") AND
+        (thCity = i_city OR i_city = "") AND
+        (thState = i_state OR i_state = "ALL");
+END
+
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `user_visit_th`(IN i_thName VARCHAR(50), IN i_comName VARCHAR(50), IN i_visitDate DATE, IN i_username VARCHAR(50))
+BEGIN
+    INSERT INTO UserVisitTheater (thName, comName, visitDate, username)
+    VALUES (i_thName, i_comName, i_visitDate, i_username);
+END
+
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `user_filter_visitHistory`(IN i_username VARCHAR(50), IN i_minVisitDate DATE, IN i_maxVisitDate DATE)
+BEGIN
+    DROP TABLE IF EXISTS UserVisitHistory;
+    CREATE TABLE UserVisitHistory
+	SELECT thName, thStreet, thCity, thState, thZipcode, comName, visitDate
+    FROM UserVisitTheater
+		NATURAL JOIN
+        Theater
+	WHERE
+		(username = i_username) AND
+        (i_minVisitDate IS NULL OR visitDate >= i_minVisitDate) AND
+        (i_maxVisitDate IS NULL OR visitDate <= i_maxVisitDate);
+END
