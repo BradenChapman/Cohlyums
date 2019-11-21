5f0ec14a2846dcbe9c77d8a01df68193fbc52e3a
David Zhou <dzhou71@gatech.edu>
2019-11-21 08:46:55 -0500

    Update Team11_SP.sql

--- Team11_SP.sql
+++ Team11_SP.sql
@@ -1,3 +1,5 @@
+--SCREEN 1--
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `user_login`(IN i_username VARCHAR(50), IN i_password VARCHAR(50))
 BEGIN
 	SELECT username, status, isCustomer, i_username in (
@@ -11,24 +13,26 @@ BEGIN
 	WHERE i_username = user.username and i_password = user.password;
 END
 
+--SCREEN 3--
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `user_register`(IN i_username VARCHAR(50), IN i_password VARCHAR(50), IN i_firstname VARCHAR(50), IN i_lastname VARCHAR(50))
 BEGIN
 		INSERT INTO user (username, password, firstname, lastname) VALUES (i_username, MD5(i_password), i_firstname, i_lastname);
 END
 
+--SCREEN 4--
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `customer_only_register`(IN i_username VARCHAR(50), IN i_password VARCHAR(50), IN i_firstname VARCHAR(50), IN i_lastname VARCHAR(50))
 BEGIN
 		INSERT INTO user (username, password, firstname, lastname, isCustomer) VALUES (i_username, MD5(i_password), i_firstname, i_lastname,1);
 END
 
-
 CREATE DEFINER=`root`@`localhost` PROCEDURE `customer_add_credicard`(IN i_username VARCHAR(50), IN i_creditCardNum CHAR(16))
 BEGIN
 		INSERT INTO customercreditcard (username, creditCardNum) VALUES (i_username, i_creditCardNum);
 END
 
+--SCREEN 5--
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `manager_only_register`(IN i_username VARCHAR(50), IN i_password VARCHAR(50), IN i_firstname VARCHAR(50), IN i_lastname VARCHAR(50), IN i_comName VARCHAR(50), IN i_empStreet VARCHAR(50), IN i_empCity VARCHAR(50), IN i_empState CHAR(2), IN i_empZipcode CHAR(5))
 BEGIN
@@ -36,6 +40,7 @@ BEGIN
         INSERT INTO manager (username, comName, manStreet, manCity, manState, manZipcode) VALUES (i_username, i_comName, i_empStreet, i_empCity, i_empState, i_empZipcode);
 END
 
+--SCREEN 6--
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `manager_customer_register`(IN i_username VARCHAR(50), IN i_password VARCHAR(50), IN i_firstname VARCHAR(50), IN i_lastname VARCHAR(50), IN i_comName VARCHAR(50), IN i_empStreet VARCHAR(50), IN i_empCity VARCHAR(50), IN i_empState CHAR(2), IN i_empZipcode CHAR(5))
 BEGIN
@@ -43,12 +48,12 @@ BEGIN
         INSERT INTO manager (username, comName, manStreet, manCity, manState, manZipcode) VALUES (i_username, i_comName, i_empStreet, i_empCity, i_empState, i_empZipcode);
 END
 
-
 CREATE DEFINER=`root`@`localhost` PROCEDURE `manager_customer_add_creditcard`(IN i_username VARCHAR(50), IN i_creditCardNum CHAR(16))
 BEGIN
 		INSERT INTO customercreditcard (username, creditCardNum) VALUES (i_username, i_creditCardNum);
 END
 
+--SCREEN 13--
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `admin_approve_user`(IN i_username VARCHAR(50))
 BEGIN
@@ -57,16 +62,12 @@ BEGIN
         WHERE username = i_username;
 END
 
-
 CREATE DEFINER=`root`@`localhost` PROCEDURE `admin_decline_user`(IN i_username VARCHAR(50))
 BEGIN
 		UPDATE user
         SET status = 'Declined'
         WHERE username = i_username;
 END
-
-
--- Last one done - Screen 13: Admin decline user
 
 DROP PROCEDURE IF EXISTS admin_filter_user;
 DELIMITER $$
@@ -102,13 +103,16 @@ BEGIN
 END$$
 DELIMITER ;
 	
+-- Last one done - Screen 13: Admin filter user
 
--- INSERT YOUR STUFF HERE
+--SCREEN 19--
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `manager_schedule_mov`(IN i_manUsername VARCHAR(50), IN i_movName VARCHAR(50), IN i_movReleaseDate DATE, IN i_movPlayDate DATE)
 BEGIN
 	INSERT INTO movieplay(thName, comName, movName, movReleaseDate, movPlayDate) VALUES((SELECT thName FROM team11.manager WHERE username = i_manUsername), (SELECT comName FROM team11.manager WHERE username = i_manUsername), i_movName, i_movReleaseDate, i_movPlayDate);
 END
 
+--SCREEN 20--
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `customer_filter_mov`(IN i_movName VARCHAR(50), IN i_comName VARCHAR(50), IN i_city VARCHAR(50), IN i_state CHAR(3), IN i_minMovPlayDate DATE, IN i_maxMovPlayDate DATE)
 BEGIN
@@ -126,13 +130,13 @@ BEGIN
         (i_maxMovPlayDate IS NULL OR movPlayDate <= i_maxMovPlayDate);
 END
 
-
 CREATE DEFINER=`root`@`localhost` PROCEDURE `customer_view_mov`(IN  i_creditCardNum CHAR(16), IN i_movName VARCHAR(50), IN i_movReleaseDate DATE, IN i_thName VARCHAR(50), IN i_comName VARCHAR(50), IN i_movPlayDate DATE)
 BEGIN
     INSERT INTO customerviewmovie (creditcardnum, thName, comName, movName, movReleaseDate, movPlayDate) 
     VALUES (i_creditCardNum, i_thName, i_comName, i_movName, i_movReleaseDate, i_movPlayDate);
 END
 
+--SCREEN 21--
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `customer_view_history`(IN i_cusUsername VARCHAR(50))
 BEGIN
@@ -145,8 +149,7 @@ BEGIN
 		(i_cusUsername = username OR i_cusUsername = "ALL");
 END
 
--- Next procedure - Screen 22: User filter theater
-
+--SCREEN 22--
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `user_filter_th`(IN i_thName VARCHAR(50), IN i_comName VARCHAR(50), IN i_city VARCHAR(50), IN i_state VARCHAR(3))
 BEGIN
@@ -161,13 +164,13 @@ BEGIN
         (thState = i_state OR i_state = "ALL");
 END
 
-
 CREATE DEFINER=`root`@`localhost` PROCEDURE `user_visit_th`(IN i_thName VARCHAR(50), IN i_comName VARCHAR(50), IN i_visitDate DATE, IN i_username VARCHAR(50))
 BEGIN
     INSERT INTO UserVisitTheater (thName, comName, visitDate, username)
     VALUES (i_thName, i_comName, i_visitDate, i_username);
 END
 
+--SCREEN 23--
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `user_filter_visitHistory`(IN i_username VARCHAR(50), IN i_minVisitDate DATE, IN i_maxVisitDate DATE)
 BEGIN
